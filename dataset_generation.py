216904c1ba630ab7d028f0e6816d70a70d20ba53
Chunyu Wang <shycw1@student.ubc.ca>
2023-03-23 23:29:27 -0700

    mass data generation

--- dataset_generation.py
+++ dataset_generation.py
@@ -11,24 +11,42 @@ map_dim = resolution * map_pixel_size  # map dimension in meters
 map_size = (map_dim * 1000) ** 2  # map size in mm^2
 
 print(f"[Info] Map size: {map_size} mm^2")
-dataset_volume = 10
+dataset_volume = 118
 
 dataset_dir = 'dataset'
 original_dir = os.path.join(dataset_dir, 'original')
 noisy_dir = os.path.join(dataset_dir, 'noisy')
-
+label_csv = os.path.join(dataset_dir, 'label.csv')
+df = pd.DataFrame(columns=['Seed Number', 'SNR', 'Grain Number', 'Mean Grain Size'])
 # Create the dataset directory and subdirectories if they don't exist
 if not os.path.exists(dataset_dir):
     os.makedirs(dataset_dir)
 if not os.path.exists(original_dir):
     os.makedirs(original_dir)
+else:
+    original_images = [f for f in os.listdir(original_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    print(f'There are {len(original_images)} images in the original directory.')
 if not os.path.exists(noisy_dir):
     os.makedirs(noisy_dir)
+else:
+    noisy_images = [f for f in os.listdir(noisy_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    print(f'There are {len(noisy_images)} images in the noisy directory.')
+existing_num = 0
+# Check if the original and noisy directories are not empty and if the label.csv file exists
+if (os.path.exists(original_dir) and os.listdir(original_dir)) and \
+   (os.path.exists(noisy_dir) and os.listdir(noisy_dir)) and \
+   os.path.exists(label_csv):
+    print("Both 'original' and 'noisy' directories are not empty, and 'label.csv' exists.")
+    df = pd.read_csv(label_csv)
+    existing_num = len(noisy_images)
+else:
+    print("One or more conditions are not met.")
+    existing_num = 0
 
-df = pd.DataFrame(columns=['Seed Number', 'SNR', 'Grain Number', 'Mean Grain Size'])
+
 
 for idx in range(dataset_volume):
-    n = np.random.randint(100, 2000)
+    n = np.random.randint(150, 2000)
     SNR = np.random.randint(20, 40)
     
     points = np.random.rand(n, 2)
@@ -67,7 +85,7 @@ for idx in range(dataset_volume):
 
     plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
 
-    fig.savefig(f"dataset/original/{idx+1}.png", dpi=map_pixel_size/10, bbox_inches='tight', pad_inches=0, format='png', transparent=True, facecolor='none')
+    fig.savefig(f"dataset/original/{existing_num+idx+1}.png", dpi=map_pixel_size/10, bbox_inches='tight', pad_inches=0, format='png', transparent=True, facecolor='none')
 
 
     vor = Voronoi(points)
@@ -88,8 +106,8 @@ for idx in range(dataset_volume):
     areas = np.array(areas)/0.6/0.6*map_dim*map_dim*1e6
 
     mean_grain_size = round(np.mean(areas),4)
-
-    image = plt.imread(f'dataset/original/{idx+1}.png')[:, :, :3]  # keep only the first three channels
+ 
+    image = plt.imread(f'dataset/original/{existing_num+idx+1}.png')[:, :, :3]  # keep only the first three channels
     # Add Gaussian noise to the image
 
     signal = np.mean(image)
@@ -102,13 +120,13 @@ for idx in range(dataset_volume):
     # print(f"[Info] SNR: {snr} dB")
 
     # Save the noisy image
-    plt.imsave(f"dataset/noisy/{idx+1}.png", noisy_image)
+    plt.imsave(f"dataset/noisy/{existing_num+idx+1}.png", noisy_image)
 
-    print(f"[Info] image {idx+1} with {n} seeds, SNR: {snr} dB, Grain Number: {number_of_centroids}, mean grain size : {mean_grain_size} mm^2 Generated")
+    print(f"[Info] image {existing_num+idx+1} with {n} seeds, SNR: {snr} dB, Grain Number: {number_of_centroids}, mean grain size : {mean_grain_size} mm^2 Generated")
 
     data_entry = {'Seed Number': int(n), 'SNR': snr, 'Grain Number': int(number_of_centroids), 'Mean Grain Size': mean_grain_size}
     data_entry_df = pd.DataFrame(data_entry, index=[0])
     df = pd.concat([df, data_entry_df], ignore_index=True)
+    df.to_csv('dataset/label.csv', index=False)
 
 # Save the DataFrame to a CSV file
-df.to_csv('dataset/label.csv', index=False)
--- test.py
+++ test.py
@@ -4,24 +4,29 @@ from scipy.spatial import Voronoi, voronoi_plot_2d
 from skimage.util import random_noise
 import imageio
 
-# Generate some random points 100 -2000
-n = 3000 
-resolution = 24e-6
-map_size = resolution*512*512*resolution
-# print(512*24e-6)
+
+map_pixel_size = 512
+resolution = 24e-6 # 24 micron 
+map_dim= resolution*map_pixel_size # map dimension in meters 
+map_size = (map_dim * 1000) ** 2 # map size in mm^2
+
+print(f"[Info] Map size: {map_size} mm^2")
+
+
+n = 100  # Generate some random points 100 -2000
+SNR = 30  # signal-to-noise ratio
+
+
+
 points = np.random.rand(n, 2)
-# print(points)
 # Generate the Voronoi diagram
 vor = Voronoi(points)
-
 # Assign a random value to each point
 values = np.random.rand(n)
-
 # Create the colormap
 cmap = plt.get_cmap('jet')
 polygons = []
 # Plot the Voronoi diagram with colored patches
-#
 fig, ax = plt.subplots(figsize=(10, 10))
 for i, region in enumerate(vor.regions):
     if not -1 in region:
@@ -30,8 +35,6 @@ for i, region in enumerate(vor.regions):
         ax.fill(*zip(*polygon), color=cmap(values[i-1]))
 
 
-# print(len(polygons))
-# Set the axis limits
 Trimming_Upper_Limit = 0.8
 Trimming_Lower_limit = 0.2
 ax.set_xlim([Trimming_Lower_limit, Trimming_Upper_Limit])
@@ -50,10 +53,8 @@ ax.set_axis_off()
 # Show the plot
 
 plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
-# fig.savefig('grain_map.png', dpi=51.2)
-fig.savefig('grain_map.png', dpi=51.2, bbox_inches='tight', pad_inches=0, format='png', transparent=True, facecolor='none')
-# plt.plot(Centroid_X, Centroid_Y, 'r.')
-# plt.show()
+
+fig.savefig('grain_map.png', dpi=map_pixel_size/10, bbox_inches='tight', pad_inches=0, format='png', transparent=True, facecolor='none')
 
 
 
@@ -70,23 +71,19 @@ for i, region in enumerate(vor.regions):
     y = [p[1] for p in polygon]
     area = 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
     areas.append(area)
-# print(len(areas))
-areas = np.array(areas)/(0.6*0.6)*map_size*1e6
 areas= np.sort(areas)
 areas = areas[int(n*0.2):int(n*0.7)]
-# print(areas)
-# for i in areas:
-#     print(i)
-# calculate the mean grain size
+areas = np.array(areas)/0.6/0.6*map_dim*map_dim*1e6
+
 mean_grain_size = np.mean(areas)
-print(f"The mean grain size is: {mean_grain_size}")
+print(f"[INFO] mean grain size is: {mean_grain_size} mm^2")
 
 
 # Load the saved image
 image = plt.imread('grain_map.png')[:, :, :3]  # keep only the first three channels
 
 # Add Gaussian noise to the image
-SNR = 30  # signal-to-noise ratio
+
 signal = np.mean(image)
 std = signal / (10**(SNR/20))
 noisy_image = random_noise(image, mode='gaussian', var=std**2)
@@ -94,7 +91,7 @@ noisy_image = random_noise(image, mode='gaussian', var=std**2)
 # Calculate the SNR
 noise = np.std(noisy_image - image)
 snr = 20 * np.log10(signal / noise)
-print(f"[Info] SNR: {snr}")
+print(f"[Info] SNR: {snr} dB")
 
 # Save the noisy image
 plt.imsave('noisy_grain_map.png', noisy_image)
